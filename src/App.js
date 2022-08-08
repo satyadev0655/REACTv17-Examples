38cc77e0a225953a4666580b290b522a31fd24da
satyadev0655 <satya.dev0655@gmail.com>
2022-08-08 20:25:38 +0530

    2 files modified, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import Dashboard from './Dashboard';
-import Preferences from './Preferences';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 
-import { useHistory } from 'react-router-dom';
-
-function HomeButton() {
-  let history = useHistory();
-
-  function handleClick() {
-    history.push('/dashboard');
-  }
-
+export default function App() {
   return (
-    <button type="button" onClick={handleClick}>
-      Go home
-    </button>
-  );
-}
-
-function App() {
-  return (
-    <div className="wrapper">
-      <h1>Application</h1>
-      <HomeButton />
-      <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+    <div>
+      <h1>Hello World!</h1>
     </div>
   );
 }
-
-export default App;
